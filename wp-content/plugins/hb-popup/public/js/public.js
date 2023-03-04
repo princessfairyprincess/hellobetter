2c9ee2436f033bd7c253c824721212c49d0ac890
jessicamichaels <jessica@touchfoundry.co.za>
2023-03-04 15:03:46 +0200

    add success state. add states to input elements. add js (incl. cookie storage)

--- wp-content/plugins/hb-popup/public/js/public.js
+++ wp-content/plugins/hb-popup/public/js/public.js
@@ -1,32 +1,93 @@
 (function( $ ) {
 	'use strict';
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+	$(document).ready(function () {
+		/**
+		 * Display/hide functionality
+		 */
+
+		//Display popup if user scrolls to 50% of the page, OR if no scroll space, appear after 10 seconds.
+
+		let container = $('.hb-popup-container');
+		let page = $('html');
+		let height = page.height();
+		let viewHeight = $(window).height();
+		let threshold = height/3;
+		let close = $('.hb-close');
+		let maxAge = 86400; //1 day (60*60*24)
+		let cookieVal = "hb-popup-closed=true, max-age="+maxAge+", SameSite=Lax" //Only show popup for as long as the maxAge value
+
+		let hbPopUpAppear = function(delay) {
+			if ((document.cookie !== cookieVal && !page.hasClass('hb-popup-closed')) ) { //If user has closed popup already, don't show again until cookie expires
+				container.addClass('anim');
+				setTimeout(() => {
+					container.addClass('active');
+					if (container.hasClass('active')) {
+						page.addClass('hb-popup-active');
+					}
+				}, delay)
+			}
+		}
+
+		if (height > viewHeight) {
+			$(window).on('scroll', function () {
+				if (scrollY >= threshold ) {
+					hbPopUpAppear(0);
+				}
+			});
+		} else {
+			hbPopUpAppear(10000);
+		}
+
+		//Close functionality
+
+		close.on('click', function () {
+			if (container.hasClass('active')) {
+				container.removeClass('active');
+				page.removeClass('hb-popup-active').addClass('hb-popup-closed');
+				document.cookie = cookieVal;
+			}
+		});
+
+		/**
+		 * Form behaviour
+		 */
+
+		//Validation
+
+		let form = $('.popup-form');
+		let field = $('#hb-email');
+		let submit = $('#hb-submit');
+		let errorText = $('.error-text');
+		let regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+		let popupCont = $('.hb-popup-inner');
+
+		$(submit).on('click', function(event) {
+			event.preventDefault();
+			submit.addClass('wait');
+			if (field.hasClass('error')) {
+				field.removeClass('error');
+			}
+
+			if (errorText.hasClass('active')) {
+				errorText.addClass('active');
+			}
+
+			if ((field).val().match(regexEmail)) {
+				setTimeout(() => {
+					submit.removeClass('wait');
+					popupCont.removeClass('cta-active').addClass('success-active');
+				}, 1000)
+			} else {
+				setTimeout(() => {
+					submit.removeClass('wait');
+					field.addClass('error');
+					errorText.addClass('active');
+				}, 1000);
+
+			}
+		});
+
+	});
 
 })( jQuery );
--- wp-content/plugins/hb-popup/public/partials/hb-popup-public.php
+++ wp-content/plugins/hb-popup/public/partials/hb-popup-public.php
@@ -23,17 +23,20 @@ $email_placeholder = "beispiel@email.de";
 $email_error = "Bitte schreibe deine Email in dieser Form: beispiel@email.de";
 $submit_label = "Senden";
 $close_label = "Schließen";
+$success_img = "/wp-content/plugins/hb-popup/public/img/hb-success-img.svg";
+$success_heading= "Perfekt, das hat geklappt!";
+$success_body = "Danke, dass du dich für unseren Newsletter angemeldet hast. Sei gespannt, dich erwarten schon bald viele spannende Strategien und Tipps aus der Welt der Psychologie und Psychotherapie!";
 ?>
 
 
-<div class="hb-popup-container active">
+<div class="hb-popup-container">
     <div class="hb-popup">
-        <div class="hb-popup-inner">
-            <div class="hb-popup-content">
+        <div class="hb-popup-inner cta-active">
+            <div class="hb-popup-content hb-cta-section">
                 <div class="hb-copy">
                     <div class="copy-inner">
                         <div class="image-container">
-                            <!--Set inline so that the image url can be taken from the $image variable - and thus can be defined by user at a later stage. I try to avoid inlining styles otherwise-->
+                            <!--Set inline so that the image url can be taken from the $image variable and so that we can use the image responsively ("crop") - and thus can be defined by user at a later stage. I try to avoid inlining styles otherwise-->
                             <div class="image" style="background-image: url('<?php echo $image; ?>');"></div>
                         </div>
                         <div class="text-container">
@@ -47,7 +50,7 @@ $close_label = "Schließen";
                             </div>
                         </div>
                         <div class="close-container desktop">
-                            <div class="close close-icon">
+                            <div class="hb-close close-icon">
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                      xmlns="http://www.w3.org/2000/svg">
                                     <path d="M5 5L12 12M19 19L12 12M12 12L19 5L5 19" stroke="#868989" stroke-width="2"
@@ -72,7 +75,44 @@ $close_label = "Schließen";
                             </form>
                         </div>
                         <div class="close-container mobile">
-                            <button class="close close-button"><?php echo $close_label; ?></button>
+                            <button class="hb-close close-button"><?php echo $close_label; ?></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="hb-popup-content hb-success-section">
+                <div class="hb-copy">
+                    <div class="copy-inner flex-col">
+                        <div class="inner-top">
+                            <div class="image-container">
+                                <img src="<?php echo $success_img; ?>" alt="<?php echo $success_heading; ?>" width="200px" height="auto">
+                            </div>
+                            <div class="close-container desktop">
+                                <div class="hb-close close-icon">
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M5 5L12 12M19 19L12 12M12 12L19 5L5 19" stroke="#868989" stroke-width="2"
+                                              stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text-container center">
+                            <div class="heading">
+                                <h1><?php echo $success_heading; ?></h1>
+                            </div>
+                            <div class="body">
+                                <p>
+                                    <?php echo $success_body; ?>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="hb-cta">
+                    <div class="cta-inner">
+                        <div class="close-container mobile">
+                            <button class="hb-close close-button"><?php echo $close_label; ?></button>
                         </div>
                     </div>
                 </div>
